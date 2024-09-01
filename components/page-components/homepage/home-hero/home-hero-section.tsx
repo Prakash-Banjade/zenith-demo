031afbf145b1e483c7d0e03563b3e6e429102a15
Prakash Banjade <prakashbanjade191@gmail.com>
2024-09-01 23:17:42 +0545

    change hero image

--- components/page-components/homepage/home-hero/home-hero-section.tsx
+++ components/page-components/homepage/home-hero/home-hero-section.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeBannerImage from "@/public/assets/banners/banner-2.jpg";
+import HomeBannerImage from "@/public/assets/banners/hero-background.webp";
 import Image from "next/image";
 import { HeroForm } from "./hero-form";
 
@@ -13,7 +13,7 @@ export const HomeHeroSection = () => {
         objectFit="cover"
         className="absolute inset-0 w-full h-full object-cover "
       />
-      <div className="absolute bg-black inset-0 w-full h-full opacity-30"></div>
+      <div className="absolute bg-black inset-0 w-full h-full opacity-50"></div>
       <div className="w-full h-full flex items-center justify-center  text-white relative z-50 lg:py-0 py-10">
         <div className="container mx-auto 2xl:space-y-14 xl:space-y-10 md:space-y-8 space-y-3 lg:pt-0 pt-[100px]">
           <div className="h-fit 2xl:text-7xl xl:text-6xl lg:text-5xl text-3xl font-semibold">
